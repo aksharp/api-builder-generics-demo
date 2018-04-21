b05ec8ddf1f61b275b3e68f71c0fb59cdd5f6a7d
Alex <alex@flow.io>
2018-04-21 16:29:54 -0400

    wip - storage (need to impl output json serialization)

--- src/main/scala/service/Main.scala
+++ src/main/scala/service/Main.scala
@@ -1,10 +1,15 @@
 package service
 
 import lib.JobRunner
-import service.generated._
+import service.generated.{MyDailyEtlJob, _}
 import lib.generated._
 import org.joda.time.DateTime
 import Implementations._
+import com.typesafe.config.{Config, ConfigFactory}
+import lib.db.JobStorage
+import lib.db.generated.JobInstancesDao
+import play.api.Environment
+import play.api.db._
 
 object Main extends App {
 
@@ -14,6 +19,13 @@ object Main extends App {
       TotalDailyRevenueByOrganization,
       JobError
     ]
+
+  val config = ConfigFactory.load()
+  val env = Environment.simple()
+  val pool = new HikariCPConnectionPool(env)
+  val db = new PooledDatabase("jobs", config, env, pool)
+  val dao = new JobInstancesDao(db)
+  val storage = new JobStorage[MyDailyEtlJob.type, Day, TotalDailyRevenueByOrganization, JobError](dao)
 
   /*
   * Compile-time Type Check
@@ -37,6 +49,8 @@ object Main extends App {
   */
   // TODO: uncomment below to see compilation error
   // jobRunner.run(MyDailyEtlJob, "Some Input")
+
+  storage.findById("123")
 
 
   println(s"Have a great day!")
