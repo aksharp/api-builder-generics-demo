ccce6ad3097878fba52cfb8c3c055e57934c67bf
Alex <alex@flow.io>
2018-04-23 00:48:30 -0400

    dao cleanup

--- src/main/scala/lib/db/generated/GeneratedGenericJobInstancesDao.scala
+++ src/main/scala/lib/db/generated/GeneratedGenericJobInstancesDao.scala
@@ -11,9 +11,73 @@ import play.api.libs.json.{JsObject, JsValue}
 import scala.language.implicitConversions
 import scala.util.Try
 
-//TODO: would be great if this can be generated
+//TODO: ---------------------------------------------------
+//TODO: -----NEW TYPED DAO & TYPE CONVERSIONS--------------
+//TODO: ---------------------------------------------------
+
+class GeneratedGenericJobInstancesDao[J, I, O, E <: JobError](
+  dao: JobInstancesDao
+) {
+
+  def insert(user: UserReference, form: JobInstanceForm[J, I, O, E])(
+    implicit
+    sj: Serializer[J, JsValue],
+    si: Serializer[Option[I], Option[JsValue]],
+    so: Serializer[Option[O], Option[JsValue]],
+    se: Serializer[Option[List[E]], Option[List[JsValue]]],
+    dsj: Deserializer[J, JsObject],
+    dsi: Deserializer[Option[I], Option[JsObject]],
+    dso: Deserializer[Option[O], Option[JsObject]],
+    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
+  ): Either[JobError, JobInstance[J, I, O, E]] = {
+    Try(dao.insert(
+      updatedBy = user,
+      form = form)
+    ).fold(
+      err => Left(JobDatabaseError(databaseName, s"insert job instance $form by user $user", err.getMessage)),
+      good => findById(good)
+    )
+  }
+
+  def findById(id: String)(
+    implicit
+    dsj: Deserializer[J, JsObject],
+    dsi: Deserializer[Option[I], Option[JsObject]],
+    dso: Deserializer[Option[O], Option[JsObject]],
+    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
+  ): Either[JobError, JobInstance[J, I, O, E]] = {
+    dao
+      .findById(id)
+      .map(fromDbJobInstance[J, I, O, E])
+      .getOrElse(
+        Left(
+          JobDatabaseError(databaseName, "find by id", s"job instance with id $id was not found")
+        )
+      )
+  }
+
+  def findLastIncomplete(key: String)(
+    implicit
+    dsj: Deserializer[J, JsObject],
+    dsi: Deserializer[Option[I], Option[JsObject]],
+    dso: Deserializer[Option[O], Option[JsObject]],
+    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
+  ): Either[JobError, JobInstance[J, I, O, E]] = {
+    dao
+      .findAll(limit = 1)(q =>
+        q.equals("key", key).orderBy("created_at desc"))
+      .headOption
+      .map(fromDbJobInstance[J, I, O, E])
+      .getOrElse(
+        Left(
+          JobDatabaseError(databaseName, "find last incomplete job instance", s"job instance with key $key was not found")
+        )
+      )
+  }
+}
+
 object GeneratedGenericJobInstancesDao {
-  private val databaseName = ConfigFactory.load().getString("db.name") // TODO: refactor
+  private val databaseName = ConfigFactory.load().getString("db.name") // refactor to typed config
 
   implicit def toDbJobInstanceForm[J, I, O, E <: JobError](form: JobInstanceForm[J, I, O, E])(
     implicit
@@ -78,66 +142,5 @@ object GeneratedGenericJobInstancesDao {
         ))
       case _ => Left(JobDatabaseError(databaseName,"deserialize job instance","could not deserialize"))
     }
-  }
-}
-
-class GeneratedGenericJobInstancesDao[J, I, O, E <: JobError](
-  dao: JobInstancesDao
-) {
-
-  def insert(user: UserReference, form: JobInstanceForm[J, I, O, E])(
-    implicit
-    sj: Serializer[J, JsValue],
-    si: Serializer[Option[I], Option[JsValue]],
-    so: Serializer[Option[O], Option[JsValue]],
-    se: Serializer[Option[List[E]], Option[List[JsValue]]],
-    dsj: Deserializer[J, JsObject],
-    dsi: Deserializer[Option[I], Option[JsObject]],
-    dso: Deserializer[Option[O], Option[JsObject]],
-    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
-  ): Either[JobError, JobInstance[J, I, O, E]] = {
-    Try(dao.insert(
-      updatedBy = user,
-      form = form)
-    ).fold(
-      err => Left(JobDatabaseError(databaseName, s"insert job instance $form by user $user", err.getMessage)),
-      good => findById(good)
-    )
-  }
-
-  def findById(id: String)(
-    implicit
-    dsj: Deserializer[J, JsObject],
-    dsi: Deserializer[Option[I], Option[JsObject]],
-    dso: Deserializer[Option[O], Option[JsObject]],
-    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
-  ): Either[JobError, JobInstance[J, I, O, E]] = {
-    dao
-      .findById(id)
-      .map(fromDbJobInstance[J, I, O, E])
-      .getOrElse(
-        Left(
-          JobDatabaseError(databaseName, "find by id", s"job instance with id $id was not found")
-        )
-      )
-  }
-
-  def findLastIncomplete(key: String)(
-    implicit
-    dsj: Deserializer[J, JsObject],
-    dsi: Deserializer[Option[I], Option[JsObject]],
-    dso: Deserializer[Option[O], Option[JsObject]],
-    dse: Deserializer[Option[List[E]], Option[List[JsObject]]]
-  ): Either[JobError, JobInstance[J, I, O, E]] = {
-    dao
-      .findAll(limit = 1)(q =>
-        q.equals("key", key).orderBy("created_at desc"))
-      .headOption
-      .map(fromDbJobInstance[J, I, O, E])
-      .getOrElse(
-        Left(
-          JobDatabaseError(databaseName, "find last incomplete job instance", s"job instance with key $key was not found")
-        )
-      )
   }
 }
